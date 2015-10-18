afb9a61737abbd93d8df2bbaa21b873420ca674c
nelzas <nelzas@cisco.com>
2015-10-18 23:14:31 +0300

    working version with output and handling empty extension

--- ext_info.py
+++ ext_info.py
@@ -8,13 +8,36 @@ in a specified folder.
 # import sys
 import argparse
 from pathlib import Path
+from collections import defaultdict
+
+
+def create_folder_data():
+    return {
+        'num of files': 0,
+        'total size': 0,
+    }
 
 
 def gather_info_from_folder(path):
     folder = Path(path)
+    files_data_by_ext = defaultdict(create_folder_data)
     for p in folder.iterdir():
         if not p.is_dir():
-            print(p, p.is_dir(), p.is_file(), p.stat().st_size, p.suffix)
+            if p.suffix == '':
+                suffix = '.'
+            else:
+                suffix = p.suffix
+            files_data_by_ext[suffix]['num of files'] += 1
+            files_data_by_ext[suffix]['total size'] += p.stat().st_size
+    return files_data_by_ext
+
+
+def display_files_data(files_data_by_ext):
+    keys = list(files_data_by_ext.keys())
+    keys.sort()
+    for k in keys:
+        print(k, files_data_by_ext[k]['num of files'], files_data_by_ext[k]['total size'])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -24,6 +47,5 @@ if __name__ == '__main__':
                         help='name of folder to scan')
 
     args = parser.parse_args()
-    print("args.path:", args.path)
-    print()
-    gather_info_from_folder(args.path)
+    files_data_by_ext = gather_info_from_folder(args.path)
+    display_files_data(files_data_by_ext)
